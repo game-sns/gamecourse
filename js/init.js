5f3a659c59b63dd11a6f024d6970f7160a451eeb
Stefano Fogarollo <sirfoga@protonmail.com>
2017-11-15 22:13:14 +0100

    add init.js with labels form loading

---
+++ js/init.js
@@ -0,0 +1,53 @@
+/**
+ * Moving effect
+ */
+$(function () {
+    var defaultOptions = {
+        moveEffect: 'blind'
+        , moveEffectOptions: {
+            direction: 'vertical'
+        }
+        , moveEffectSpeed: 'slow'
+    };
+    var widgets = {
+        'simple': $.extend($.extend({}, defaultOptions), {
+            sortMethod: 'normal'
+            , sortable: true
+        })
+    };
+    $.each(widgets, function (k, i) {
+        $('#multiselect_' + k).multiselect(i).on('multiselectChange', function (evt, ui) {
+            var values = $.map(ui.optionElements, function (opt) {
+                return $(opt).attr('value');
+            }).join(', ');
+            $('#debug_' + k).prepend($('<div></div>').text('Multiselect change event! ' + (ui.optionElements.length == $('#multiselect_' + k).find('option').size() ? 'all ' : '') + (ui.optionElements.length + ' value' + (ui.optionElements.length > 1 ? 's were' : ' was')) + ' ' + (ui.selected ? 'selected' : 'deselected') + ' (' + values + ')'));
+        }).on('multiselectSearch', function (evt, ui) {
+            $('#debug_' + k).prepend($('<div></div>').text('Multiselect beforesearch event! searching for "' + ui.term + '"'));
+        }).closest('form').submit(function (evt) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            $('#debug_' + k).prepend($('<div></div>').text("Submit query = " + $(this).serialize()));
+            return false;
+        });
+        $('#btnSearch_' + k).click(function () {
+            $('#multiselect_' + k).multiselect('search', $('#txtSearch_' + k).val());
+        });
+    });
+});
+
+
+function populateLabelsTable(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'text',
+        success: function (data) {
+            $("#multiselect_simple").html(data);
+        },
+        error: function (e) {
+            console.log(e.message);
+        }
+    })
+}
+
+populateLabelsTable("data/labels_selector.txt");
